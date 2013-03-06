2776db952fb572a475974b2d2423e1b935206a0e
Art-Wolf <john.c.doyle@gmail.com>
2013-03-05 21:59:26 -0500

    Refactored html pages, moved website files into their own directory.

--- website/includes/footer.php
+++ website/includes/footer.php
@@ -1,41 +1,3 @@
-<!DOCTYPE html><!-- saved from url=(0066)http://twitter.github.com/bootstrap/examples/starter-template.html -->
-<html lang="en">
-   <head>
-      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-      <meta charset="utf-8">
-      <title>Balor</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta name="description" content="Bringing your order system to the next generation.">
-      <meta name="author" content="Balor LLC">
-      <!-- Le styles --> 
-      <link href="bootstrap.css" rel="stylesheet">
-      <link href="bootstrap-responsive.css" rel="stylesheet">
-      <link href="elastislide.css" rel="stylesheet">
-      <link href="fancybox.css" rel="stylesheet">
-      <link href="animate.css" rel="stylesheet">
-      <link href="style.css" rel="stylesheet">
-      <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements --> <!--[if lt IE 9]> <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script> <![endif]--> <!-- Le fav and touch icons --> 
-      <link rel="shortcut icon" href="http://twitter.github.com/bootstrap/assets/ico/favicon.ico">
-      <link rel="apple-touch-icon-precomposed" sizes="144x144" href="http://twitter.github.com/bootstrap/assets/ico/apple-touch-icon-144-precomposed.png">
-      <link rel="apple-touch-icon-precomposed" sizes="114x114" href="http://twitter.github.com/bootstrap/assets/ico/apple-touch-icon-114-precomposed.png">
-      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="http://twitter.github.com/bootstrap/assets/ico/apple-touch-icon-72-precomposed.png">
-      <link rel="apple-touch-icon-precomposed" href="http://twitter.github.com/bootstrap/assets/ico/apple-touch-icon-57-precomposed.png">
-   </head>
-   <body>
-      <!-- Beginning of Content ================================================== -->  
-      <div class="container">
-         <header>
-            <!-- Title and Subtitle Section --> 
-            <h1>Balor</h1>
-            <p>Lets take the annoyance of queueing out of ordering a drink. Stop the worry that your tab is growing out of control. Subscribe to be notified when it's available, or follow us on <a href="http://twitter.com/BalorTech">Twitter.</a></p>
-            <div class="hero-unit">
-               <!-- Newsletter form : Keep the id : newsletter on form--> 
-               <form class="form-horizontal" id="newsletter" action="mail.php">
-                  <div class="control-group">
-                     <div class="controls"> <input type="email" name="email" placeholder="Enter your email address to be notified"> <input type="submit" value="Go !"> </div>
-                  </div>
-               </form>
-               <!-- end #newsletter -->
             </div>
             <!-- end .hero-unit -->
          </header>
@@ -141,10 +103,10 @@
       </div>
       <!-- /container --> <!-- Javascript ================================================== --> 
     <!-- Placed at the end of the document so the pages load faster --> 
-	  <script src="jquery.js"></script> 
-	  <script src="bootstrap.js"></script> 
-	  <script src="elastislide.js"></script>  
-	  <script src="fancybox.js"></script>  
-	  <script src="scripts.js"></script>
+	  <script src="js/jquery.js"></script> 
+	  <script src="js/bootstrap.js"></script> 
+	  <script src="js/elastislide.js"></script>  
+	  <script src="js/fancybox.js"></script>  
+	  <script src="js/scripts.js"></script>
    </body>
 </html>
---
+++ website/includes/top.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html><!-- saved from url=(0066)http://twitter.github.com/bootstrap/examples/starter-template.html -->
+<html lang="en">
+   <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+      <meta charset="utf-8">
+      <title>Balor</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta name="description" content="Bringing your order system to the next generation.">
+      <meta name="author" content="Balor LLC">
+      <!-- Le styles --> 
+      <link href="/css/bootstrap.css" rel="stylesheet">
+      <link href="/css/bootstrap-responsive.css" rel="stylesheet">
+      <link href="/css/elastislide.css" rel="stylesheet">
+      <link href="/css/fancybox.css" rel="stylesheet">
+      <link href="/css/animate.css" rel="stylesheet">
+      <link href="/css/style.css" rel="stylesheet">
+      <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements --> <!--[if lt IE 9]> <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script> <![endif]--> <!-- Le fav and touch icons --> 
+      <link rel="shortcut icon" href="http://twitter.github.com/bootstrap/assets/ico/favicon.ico">
+      <link rel="apple-touch-icon-precomposed" sizes="144x144" href="http://twitter.github.com/bootstrap/assets/ico/apple-touch-icon-144-precomposed.png">
+      <link rel="apple-touch-icon-precomposed" sizes="114x114" href="http://twitter.github.com/bootstrap/assets/ico/apple-touch-icon-114-precomposed.png">
+      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="http://twitter.github.com/bootstrap/assets/ico/apple-touch-icon-72-precomposed.png">
+      <link rel="apple-touch-icon-precomposed" href="http://twitter.github.com/bootstrap/assets/ico/apple-touch-icon-57-precomposed.png">
+   </head>
+   <body>
+      <!-- Beginning of Content ================================================== -->  
+      <div class="container">
+         <header>
+            <!-- Title and Subtitle Section --> 
+            <h1>Bal<span style="color:red">o</span>r</h1>
+            <p>Lets take the annoyance of queueing out of ordering a drink. Stop the worry that your tab is growing out of control. Subscribe to be notified when it's available, or follow us on <a href="http://twitter.com/BalorTech">Twitter.</a></p>
+            <div class="hero-unit">
