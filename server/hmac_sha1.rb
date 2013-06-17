896e0f5a0314a2a7f6519c7855cf7c4cda6b4ebe
Art-Wolf <john.c.doyle@gmail.com>
2013-06-17 19:47:47 -0400

    Initial Cucumber Test Framework

--- server/hmac_sha1.rb
+++
@@ -1,8 +0,0 @@
-require 'base64'
-require 'cgi'
-require 'openssl'
-
-key = '1234'
-signature = 'abcdef'
-puts CGI.escape(Base64.encode64("#{OpenSSL::HMAC.digest('sha1',key, signature)}\n"))
-
--- server/test.sh
+++
@@ -1,1 +0,0 @@
-curl -i -X PUT -d '{"test":[0,"data"]}' localhost:4567/users/1234
