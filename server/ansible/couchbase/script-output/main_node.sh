2d72d48dc3b673ac2b711f002bcbdaad2ff374ce
Art-Wolf <john.c.doyle@gmail.com>
2014-03-23 11:35:21 -0400

    Ansible via PIP

---
+++ server/ansible/couchbase/script-output/main_node.sh
@@ -0,0 +1,1 @@
+/opt/couchbase/bin/couchbase-cli cluster-init -c 127.0.0.1:8091  --cluster-init-username=Administrator --cluster-init-password=password --cluster-init-port=8091 --cluster-init-ramsize=1024
